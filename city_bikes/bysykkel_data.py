4b7ff334958960cb2a8885fbb334c90c589ede1c
malinhl <malinhl91@gmail.com>
2020-10-12 16:37:53 +0200

    added function that gets data from OBS and sends to db

---
+++ city_bikes/bysykkel_data.py
@@ -0,0 +1,43 @@
+
+import os
+import psycopg2
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# Database variabler
+PASS = os.environ['PASS_P']
+USER_NAME = os.environ['USER_NAME_P']
+HOST = os.environ['HOST_P']
+DATABASE = os.environ['DB_TEAM']
+
+
+date= "2020/09.json"
+
+def get_month_to_db(date, tablename):
+    '''Function to load data from website as json, save it as dataframe,
+        and send the dataframe to postgres-db'''
+    
+    OBS_ROOT= "https://data.urbansharing.com/oslobysykkel.no/trips/v1/"
+    resource = requests.get(OBS_ROOT + date)
+    
+    data = resource.json()
+    df = pd.DataFrame(data)
+    
+    # Create engine for db-connection:
+    engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+    
+    # To db
+    try:
+        df.to_sql(tablename, engine, index=True, schema='bysykkel', if_exists='replace', chunksize=1000)
+        print("Sending data to db!")
+        
+    except:
+        print("Woops!")
+        
+    return df
+
+# Tester funksjon for å kjøre 1mnd med data til egen postgres-db      
+get_month_to_db(date, 'sep_obs')
+    
