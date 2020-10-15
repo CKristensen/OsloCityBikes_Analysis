876db0a9f569beea6b2b1d6ae7ced09db0a9b1e8
C. Kristensen <cjckris@gmail.com>
2020-10-15 22:20:56 +0200

    get_elevation now fetches more stations
    added also functionality to bysykkel_data to fetch historic data before 2019.

--- city_bikes/bysykkel_data.py
+++ city_bikes/bysykkel_data.py
@@ -2,17 +2,127 @@ import os
 import psycopg2
 import requests
 import pandas as pd
+import time
 from sqlalchemy import create_engine
-
+from zipfile import ZipFile
 
 # Database variabler
-PASS = os.environ['PASS_P']
-USER_NAME = os.environ['USER_NAME_P']
-HOST = os.environ['HOST_P']
-DATABASE = os.environ['DB_TEAM']
+# PASS = os.environ['PASS_P']
+# USER_NAME = os.environ['USER_NAME_P']
+# HOST = os.environ['HOST_P']
+# DATABASE = os.environ['DB_TEAM']
+
+def get_legacy_month_to_db(date, tablename):
+    '''Function to load data from website as json, save it as dataframe,
+        and send the dataframe to postgres-db'''
+    
+    OBS_ROOT= "https://data-legacy.urbansharing.com/oslobysykkel.no/"
+    resource = requests.get(OBS_ROOT + date)
+    data = pd.DataFrame()
+
+    with open('test_legacy.zip', 'ab') as fp:
+        b = resource.content
+        fp.write(b)
+
+    with ZipFile('test_legacy.zip') as myzip:
+        myzip.extractall()
+        json_files = [pos_json for pos_json in os.listdir(os.getcwd()) if pos_json.endswith('.csv')]
+        for json in json_files:
+            print(json_files)
+            test_json = pd.read_csv(str(os.getcwd()).replace('\\', '/')+'/' + str(json))
+            data = test_json
+            os.remove(json)
+            break
+    os.remove('test_legacy.zip')
+
+    data['Start time'] = data['Start time'].apply(lambda x: x[:-6])
+    data['End time'] = data['End time'].apply(lambda x: x[:-6])
+    print(str(date)+' has rows: '+ str(len(data.index)))
+    # Create engine for db-connection:
+    engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+    data.to_sql(tablename, engine, index=False, schema='bysykkel', if_exists='append', method='multi', chunksize=1000)
+    print("Sending data to db!")
+    
+    return 1
+
+def get_all_stations(tablename='stations'):
+    '''Function to load data from website as json, save it as dataframe,
+    and send the dataframe to postgres-db'''
+    all_stations = pd.DataFrame(columns=['end_station_id', 'end_station_name', 'end_station_description', 'end_station_latitude', 'end_station_longitude'])
+    
+    for date in iter_dates():
+        OBS_ROOT= "https://data.urbansharing.com/oslobysykkel.no/trips/v1/"
+        resource = requests.get(OBS_ROOT + date)
+        data = resource.json()
+        try:
+            df = pd.DataFrame(data)
+            df = df[['end_station_id', 'end_station_name', 'end_station_description', 'end_station_latitude', 'end_station_longitude']]
+            df = df.drop_duplicates(subset=['end_station_id'])
+        except:
+            print('passed '+ str(date))
+            df = pd.DataFrame()
+        
+        all_stations = pd.concat([all_stations, df])
+        all_stations = all_stations.drop_duplicates(subset=['end_station_id'])
+            # Create engine for db-connection:
+            # engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+            # df.to_sql(tablename, engine, index=False, schema='bysykkel', if_exists='append', method='multi', chunksize=1000)
+            # print("Sending data to db!")
+            
+    return all_stations
 
 
-date= "2020/09.json"
+
+
+
+
+
+
+
+
+
+
+def get_locations_legacy():
+    URL_LEG_LOC = 'https://data-legacy.urbansharing.com/legacy_station_locations.csv'
+    resource = requests.get(URL_LEG_LOC)
+    # data = pd.read_csv(resource.content)
+    parse = str(resource.content).split('\\n')
+    data_leg = pd.DataFrame()
+    rows = []
+    for row in parse[1:-1]:
+        rows.append(row.split(','))
+    data_leg = pd.DataFrame(rows, columns=['leg_id', 'latitude', 'longitude'])
+
+    OBS_ROOT= "https://data-legacy.urbansharing.com/legacy_new_station_id_mapping.csv"
+    resource = requests.get(OBS_ROOT)
+    
+    parse = str(resource.content).split('\\n')
+    data = pd.DataFrame()
+    rows = []
+    for row in parse[1:-1]:
+        rows.append(row.split(','))
+    data = pd.DataFrame(rows, columns=['new', 'leg_id'])
+
+    data_leg = data_leg.merge(data,how='inner', on=['leg_id'])
+    
+    # # Create engine for db-connection:
+    # engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+    # data.to_sql('new_legacy_table', engine, index=False, schema='bysykkel', if_exists='replace', method='multi', chunksize=1000)
+    # print("Sending data to db!")
+    
+    data_leg.set_index('new').to_excel('KJSHDKJBHASD.xls')
+    return 1
+
+
+
+
+
+
+
+
+
+
+
 
 def get_month_to_db(date, tablename):
     '''Function to load data from website as json, save it as dataframe,
@@ -20,23 +130,71 @@ def get_month_to_db(date, tablename):
     
     OBS_ROOT= "https://data.urbansharing.com/oslobysykkel.no/trips/v1/"
     resource = requests.get(OBS_ROOT + date)
-    
-    data = resource.json()
-    df = pd.DataFrame(data)
-    
-    # Create engine for db-connection:
-    engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
-    
-    # To db
     try:
-        df.to_sql(tablename, engine, index=False, schema='bysykkel', if_exists='replace', chunksize=1000)
+        data = resource.json()
+        df = pd.DataFrame(data)
+        df = df.drop(columns=['end_station_name', 'end_station_description', 'end_station_latitude', 'end_station_longitude', 'start_station_name', 'start_station_description', 'start_station_latitude', 'start_station_longitude', 'duration'])
+
+        # Create engine for db-connection:
+        engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+        df.to_sql(tablename, engine, index=False, schema='bysykkel', if_exists='append', method='multi', chunksize=1000)
         print("Sending data to db!")
-        
     except:
-        print("Woops!")
-        
+        print('passed '+ str(date))
+           
     return df
 
-# Tester funksjon for å kjøre 1mnd med data til egen postgres-db      
-get_month_to_db(date, 'sep_obs')
-    
+def iter_dates(legacy=False):
+    if(legacy==False):
+        for year in range(2019, 2021):
+            for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
+                if((year == 2019) & (int(month) < 4)):
+                    continue
+                if((year == 2020) & (int(month)==11)):
+                    break
+                yield f'{year}/{month}.json'
+    else:
+        for year in range(2016, 2019):
+            for month in ['03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
+                if(((year == 2016)|(year == 2017)) & (int(month) == 3)):
+                    continue
+                else:
+                    yield f'{year}/{month}.csv.zip'
+
+# Inserting legacy-data into D
+
+
+def legacy_ny_table_insert():
+        
+    OBS_ROOT= "https://data-legacy.urbansharing.com/legacy_new_station_id_mapping.csv"
+    resource = requests.get(OBS_ROOT)
+    # data = pd.read_csv(resource.content)
+    parse = str(resource.content).split('\\n')
+    data = pd.DataFrame()
+    rows = []
+    for row in parse[1:-1]:
+        rows.append(row.split(','))
+    data = pd.DataFrame(rows, columns=['new', 'legacy'])
+    # Create engine for db-connection:
+    engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+    data.to_sql('new_legacy_table', engine, index=False, schema='bysykkel', if_exists='replace', method='multi', chunksize=1000)
+    print("Sending data to db!")
+    return 1
+
+
+# # Inserting non-legacy data into DW
+# total_start_time = time.time()
+# for date in iter_dates():
+#     start_time = time.time()
+#     get_month_to_db(date, 'obos_data')
+#     print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s total seconds ---" % (time.time() - total_start_time))
+
+# total_start_time = time.time()
+# for date in iter_dates(True):
+#     print(get_legacy_month_to_db(date, 'obos_data_legacy'))
+# print("--- %s total seconds ---" % (time.time() - total_start_time))
+
+# legacy_ny_table_insert()
+
+get_locations_legacy()
--- city_bikes/elevation_api.py
+++ city_bikes/elevation_api.py
@@ -1,4 +1,3 @@
-
 import os
 import psycopg2
 import requests
@@ -9,18 +8,18 @@ import time
 
 
 # Database variabler
-PASS = os.environ['PASS_P']
-USER_NAME = os.environ['USER_NAME_P']
-HOST = os.environ['HOST_P']
-DATABASE = os.environ['DB_TEAM']
+PASS ="malin"
+USER_NAME ="student_malin"
+HOST ="ds-etl-academy.cgbivchwjzle.eu-west-1.rds.amazonaws.com"
+DATABASE = "gp_camaca"
 
 
-# Loading file and making subset
-df_okt= pd.read_csv('10.csv')
-df=df_okt[['start_station_id','start_station_name','start_station_latitude','start_station_longitude']].copy()
+# # Loading file and making subset
+# df_okt= pd.read_csv('10.csv')
+# df=df_okt[['start_station_id','start_station_name','start_station_latitude','start_station_longitude']].copy()
 
-# Group-by to get individual columns
-group= df.groupby(by=['start_station_id','start_station_name','start_station_latitude','start_station_longitude']).count().reset_index()
+# # Group-by to get individual columns
+# group= df.groupby(by=['start_station_id','start_station_name','start_station_latitude','start_station_longitude']).count().reset_index()
 
 
 
@@ -28,24 +27,63 @@ def get_elevation(df, lat_col, lon_col):
     
     api=[]
     elevation=[]
-    
+    print(len(df.index))
+    i = 0
     for lat, lon in zip(df[lat_col], df[lon_col]):
-        API= requests.get(f'https://api.opentopodata.org/v1/test-dataset?locations={lat},{lon}')
+        time.sleep(1.5)
+        print(i)
+        i+=1
+        API= requests.get(f'https://api.opentopodata.org/v1/test-dataset?locations={lat},{lon}', timeout=5)
         print(API)
-        time.sleep(2)
-        data=API.json()
-        print(data['results'])
-        
-        api.append(data['results'])  
-        
+        try:
+            data=API.json()
+            print(data['results'])
+            api.append(data['results'])  
+        except:
+            pass
+
     for idx, el in enumerate(api):
         print(idx)
         elevation.append(el[0]['elevation'])  
             
     return elevation, api
 
+
+
+def get_locations_legacy():
+    URL_LEG_LOC = 'https://data-legacy.urbansharing.com/legacy_station_locations.csv'
+    resource = requests.get(URL_LEG_LOC)
+    # data = pd.read_csv(resource.content)
+    parse = str(resource.content).split('\\n')
+    data_leg = pd.DataFrame()
+    rows = []
+    for row in parse[1:-1]:
+        rows.append(row.split(','))
+    data_leg = pd.DataFrame(rows, columns=['leg_id', 'latitude', 'longitude'])
+
+    OBS_ROOT= "https://data-legacy.urbansharing.com/legacy_new_station_id_mapping.csv"
+    resource = requests.get(OBS_ROOT)
+    
+    parse = str(resource.content).split('\\n')
+    data = pd.DataFrame()
+    rows = []
+    for row in parse[1:-1]:
+        rows.append(row.split(','))
+    data = pd.DataFrame(rows, columns=['new', 'leg_id'])
+
+    data_leg = data_leg.merge(data,how='inner', on=['leg_id'])
+    
+    # # Create engine for db-connection:
+    # engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
+    # data.to_sql('new_legacy_table', engine, index=False, schema='bysykkel', if_exists='replace', method='multi', chunksize=1000)
+    # print("Sending data to db!")
+    
+    return data_leg
+
+
 # Testing function:
-testing,hoyde= get_elevation(group,'start_station_latitude','start_station_longitude')
+group = get_locations_legacy()
+testing,hoyde= get_elevation(group,'latitude','longitude')
 
 # Creating and concating df
 e=pd.DataFrame(testing,columns=['elevation'])
@@ -53,8 +91,8 @@ df_station=pd.concat([group, e],axis=1)
 
 # Sending to sql-db:
 engine = create_engine(f'postgresql+psycopg2://{USER_NAME}:{PASS}@{HOST}/{DATABASE}')
-    
-df_station.to_sql('elevation', engine, index=False, schema='bysykkel',
+print('Sending')
+df_station.to_sql('all_elevation', engine, index=False, schema='bysykkel',
                   if_exists='replace', method='multi', chunksize=1000)
 
 
